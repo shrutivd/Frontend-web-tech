93255e12a759e8b229287c6ebfe961dd41c3a307
shrutivd <svd@pdx.edu>
2023-10-17 16:47:39 -0700

    completed form along with javascript and completed backgroud css

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,8 @@
 // Add your code here
+let nIntervId;
+
+setInterval(function(){
+            var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+            document.body.style.backgroundColor = "#" + randomColor;
+        }, 1000)
+    
--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,20 @@
-// Add your code here
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const display_name = document.getElementById("name").value;
+  const display_email = document.getElementById("email").value;
+  const display_status = document.getElementById("status").value;
+
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+  let display_checkboxes = Array.from(checkboxes).map((checkbox) => checkbox.labels[0].textContent).join(", ");
+  const display_other = document.getElementById("textArea").value;
+
+  console.group("=========Form Submission=========");
+  console.log("Full Name: ", display_name);
+  console.log("Email: ", display_email);
+  console.log("Registration Status: ", display_status);
+  console.log("Courses taken: ", display_checkboxes);
+  console.log("Anything else: ", display_other);
+  console.groupEnd();
+});
