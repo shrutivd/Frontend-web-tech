351cf235d6cc52dc45c99800456f70b72246cc0d
shrutivd <svd@pdx.edu>
2023-10-18 08:57:34 -0700

    preetier changes

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,30 +1,29 @@
-let interval_id; // Variable to store the interval ID
+let interval_id;
 
-// Function to generate a random rgba color with a specified alpha value
 function randomRGBA() {
-    const red = Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256);
-    const alpha = Math.random();
-    return `rgba(${red},${green},${blue},${alpha})`;
+	const red = Math.floor(Math.random() * 256);
+	const green = Math.floor(Math.random() * 256);
+	const blue = Math.floor(Math.random() * 256);
+	const alpha = Math.random();
+	return `rgba(${red},${green},${blue},${alpha})`;
 }
 
-
 function setBackgroundColour() {
-    document.body.style.backgroundColor = randomRGBA();
+	document.body.style.backgroundColor = randomRGBA();
 }
 
 interval_id = setInterval(setBackgroundColour, 3000);
 
-const button = document.querySelector('button');
-button.addEventListener('click', function() {
-    if (interval_id) {
-        clearInterval(interval_id);
-        interval_id = null;
-        button.textContent = 'Start'; 
-    } else {
-        const interval = parseInt(document.getElementById('interval').value, 10) * 1000;
-        interval_id = setInterval(setBackgroundColour, interval);
-        button.textContent = 'Stop';
-    }
+const button = document.querySelector("button");
+button.addEventListener("click", function () {
+	if (interval_id) {
+		clearInterval(interval_id);
+		interval_id = null;
+		button.textContent = "Start";
+	} else {
+		const interval =
+			parseInt(document.getElementById("interval").value, 10) * 1000;
+		interval_id = setInterval(setBackgroundColour, interval);
+		button.textContent = "Stop";
+	}
 });
